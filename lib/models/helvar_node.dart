233895838f00d3f1603b6fe0e5e5416003f65620
yalpay <yasinalpay@elekon-tr.com>
2025-05-02 16:38:17 +0300

    tree view node added

---
+++ lib/models/helvar_node.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_treeview/flutter_simple_treeview.dart';
+
+enum TreeViewNodeType {
+  group,
+  inputDevice,
+  inputDeviceNode,
+  outputDevice,
+  outputDeviceNode,
+}
+
+class TreeViewNode extends TreeNode {
+  final String id;
+  final String name;
+  final TreeViewNodeType type;
+
+  TreeViewNode({
+    required this.id,
+    required this.name,
+    required this.type,
+    super.children,
+  }) : super(content: Text(name));
+}
