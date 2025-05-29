d4b6bd85768e1a9349ccfcbfd43ecb9a90f622a7
yalpay <yasinalpay@elekon-tr.com>
2025-05-29 05:54:24 +0300

    some implementations done in refactor

--- lib/utils/component_factory.dart
+++ lib/utils/component_factory.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:grms_designer/niagara/controllers/canvas_interaction_controller.dart';
 import 'package:grms_designer/niagara/controllers/flow_editor_state.dart';
+import 'package:grms_designer/niagara/home/command.dart';
 import 'package:grms_designer/niagara/home/manager.dart';
+import 'package:grms_designer/niagara/models/component.dart';
 import 'package:grms_designer/niagara/models/component_type.dart';
+import 'package:grms_designer/utils/canvas_utils.dart';
 import 'package:grms_designer/utils/persistent_helper.dart';
 
 void addNewComponent(
@@ -14,7 +17,44 @@ void addNewComponent(
   Function() updateCanvasSize, {
   Offset? clickPosition,
 }) {
-  // move implementation here
+  String baseName = type.toString();
+  int counter = 1;
+  String newName = '$baseName $counter';
+
+  while (flowManager.components.any((comp) => comp.id == newName)) {
+    counter++;
+    newName = '$baseName $counter';
+  }
+
+  Component newComponent = flowManager.createComponentByType(
+    newName,
+    type.type,
+  );
+
+  Offset newPosition =
+      clickPosition ?? getDefaultPosition(canvasController, editorState);
+
+  Map<String, dynamic> state = {
+    'position': newPosition,
+    'key': editorState.getComponentKey(newComponent.id),
+    'positions': editorState.componentPositions,
+    'keys': editorState.componentKeys,
+  };
+
+  final command = AddComponentCommand(flowManager, newComponent, state);
+  editorState.commandHistory.execute(command);
+
+  editorState.initializeComponentState(
+    newComponent,
+    position: newPosition,
+    width: 160.0,
+  );
+
+  persistenceHelper.saveAddComponent(newComponent);
+  persistenceHelper.saveComponentPosition(newComponent.id, newPosition);
+  persistenceHelper.saveComponentWidth(newComponent.id, 160.0);
+
+  updateCanvasSize();
 }
 
 void addNewDeviceComponent(
--- lib/utils/dialog_utils.dart
+++ lib/utils/dialog_utils.dart
@@ -1,5 +1,3 @@
-// utils/dialog_utils.dart
-
 import 'package:flutter/material.dart';
 import 'package:grms_designer/niagara/controllers/canvas_interaction_controller.dart';
 import 'package:grms_designer/niagara/controllers/clipboard_manager.dart';
@@ -11,16 +9,113 @@ void showAddComponentDialogAtPosition(
   Offset position,
   Function(ComponentType, {Offset? clickPosition}) addNewComponent,
 ) {
-  // move implementation here
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        child: SizedBox(
+          width: 400,
+          height: 600,
+          child: Column(
+            children: [
+              AppBar(
+                title: const Text('Add Component'),
+                automaticallyImplyLeading: false,
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      buildComponentCategorySection(
+                        context,
+                        'Devices',
+                        ['DALI_DEVICE', 'RCM_DEVICE'],
+                        position,
+                        addNewComponent,
+                      ),
+                      buildComponentCategorySection(
+                        context,
+                        'Inputs',
+                        ['BUTTON_POINT', 'VARIABLE'],
+                        position,
+                        addNewComponent,
+                      ),
+                      buildComponentCategorySection(
+                        context,
+                        'Logics',
+                        ['OR', 'AND', 'NOT', 'DELAY', 'LATCH'],
+                        position,
+                        addNewComponent,
+                      ),
+                      buildComponentCategorySection(
+                        context,
+                        'Controls',
+                        ['SLIDER', 'BUTTON'],
+                        position,
+                        addNewComponent,
+                      ),
+                      buildComponentCategorySection(
+                        context,
+                        'Outputs',
+                        ['OUTPUT'],
+                        position,
+                        addNewComponent,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
 
 Widget buildComponentCategorySection(
+  BuildContext context,
   String title,
   List<String> typeStrings,
   Offset position,
   Function(ComponentType, {Offset? clickPosition}) addNewComponent,
 ) {
-  // move implementation here
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          title,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Wrap(
+        children: typeStrings.map((typeString) {
+          return InkWell(
+            onTap: () {
+              ComponentType type = ComponentType.fromString(typeString);
+              addNewComponent(type, clickPosition: position);
+              Navigator.of(context).pop();
+            },
+            child: SizedBox(
+              width: 100,
+              height: 100,
+              child: Card(child: Center(child: Text(typeString))),
+            ),
+          );
+        }).toList(),
+      ),
+    ],
+  );
 }
 
 void showPasteSpecialDialog(
